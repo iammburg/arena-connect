b9fee514c0066c2842b0508e79fe77280c4080eb
ratnaanjalie <ratnaanjalie86@gmail.com>
2024-10-23 22:01:57 +0700

    add and update detail_pembayaran.dart

---
+++ lib/screens/booking/detail_pembayaran.dart
@@ -0,0 +1,339 @@
+import 'package:flutter/material.dart';
+import 'package:arena_connect/config/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PaymentScreen(),
+    );
+  }
+}
+
+class PaymentScreen extends StatefulWidget {
+  @override
+  _PaymentScreenState createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(150), 
+        child: Container(
+          color: primary, 
+          child: Stack(
+            children: [
+              Positioned(
+                top: 55, 
+                left: 0,
+                right: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center, 
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        scheduleChooseIcon(), 
+                        Text(
+                          'Pilih Jadwal',
+                          style: TextStyle(color: Colors.white), 
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 10), 
+                    Column(
+                      children: [
+                        buildDashedLine(), 
+                        SizedBox(height: 20), 
+                      ],
+                    ),
+                    SizedBox(width: 10), 
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center, 
+                      children: [
+                        bookingDetailIcon(), 
+                        Text(
+                          'Detail Booking',
+                          style: TextStyle(color: Colors.white), 
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 10), 
+                    Column(
+                      children: [
+                        buildDashedLine(), 
+                        SizedBox(height: 20), 
+                      ],
+                    ),
+                    SizedBox(width: 10), 
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center, 
+                      children: [
+                        paymentIcon(), 
+                        Text(
+                          'Pembayaran',
+                          style: TextStyle(color: Colors.white), 
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 20, 
+                left: 10, 
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back, color: white),
+                  onPressed: () {
+                    Navigator.pop(context); 
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView( 
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20), 
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: white, 
+                borderRadius: BorderRadius.circular(15), 
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row( 
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Menunggu Pembayaran',
+                        style: superFont1.copyWith(color: primary), 
+                      ),
+                      Icon(Icons.access_time, color: primary),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  Row( 
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Selesaikan pembayaran sebelum',
+                        style: regulerFont1.copyWith(color: primary), 
+                      ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Text(
+                        '00:59:11',
+                        style: regulerFont1.copyWith(color: primary), 
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+                  SizedBox(height: 15),
+                  Divider(color: Colors.grey.shade300), 
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Order ID',
+                        style: superFont4.copyWith(color: primary), 
+                      ),
+                      Text(
+                        'XXXXXXXX',
+                        style: superFont4.copyWith(color: primary),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10), 
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 10,
+                          offset: Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 80, 
+                          child: Center(
+                            child: Text(
+                              'BRI',
+                              style: buttonFont1.copyWith(color: primary), 
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 1,
+                          height: 30, 
+                          color: Colors.grey.shade300,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            'BAYAR MELALUI BRI',
+                            style: buttonFont2.copyWith(color: primary), 
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Divider(color: Colors.grey.shade300),
+                  SizedBox(height: 10),
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  Text(
+                    'Total Pembayaran',
+                    style: superFont3.copyWith(color: primary),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Rp30.000',
+                    style: superFont3.copyWith(color: primary), 
+                  ),
+                  ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: GestureDetector(
+                onTap: () {
+                  // Upload Bukti Pembayaran action
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 25),
+                          child: Text(
+                          'Upload Bukti Pembayaran',
+                          style: superFont2.copyWith(color: primary),
+                        ),
+                        ),
+                        SizedBox(width: 162),
+                        Icon(Icons.upload_file, color: primary), 
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget scheduleChooseIcon() {
+    return IconButton(
+      icon: Icon(Icons.calendar_month_outlined),
+      color: primary,
+      onPressed: null,
+      disabledColor: Colors.white,
+    );
+  }
+
+
+  Widget bookingDetailIcon() {
+    return IconButton(
+      icon: Icon(Icons.list_alt_rounded),
+      color: primary,
+      onPressed: null,
+      disabledColor: Colors.white,
+    );
+  }
+
+
+  Widget paymentIcon() {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white, 
+            border: Border.all(color: Colors.white, width: 1),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.payments_outlined),
+            color: primary, 
+            onPressed: null,
+            disabledColor: primary,
+          ),
+        ),
+      ],
+    );
+  }
+
+
+  Widget buildDashedLine() {
+    return Container(
+      width: 60, 
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Flex(
+            children: List.generate(
+             10,
+              (index) => SizedBox(
+                width: 3,
+                height: 2, 
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.grey), 
+                ),
+              ),
+            ),
+            direction: Axis.horizontal,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          );
+        },
+      ),
+    );
+  }
+}
