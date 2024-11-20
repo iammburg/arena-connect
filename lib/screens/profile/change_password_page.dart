8c5c052a12b95393663f0d3532dbe9c8efd1b54a
ratnaanjalie <ratnaanjalie86@gmail.com>
2024-11-20 10:52:39 +0700

    feat : edit page change_password

--- lib/screens/profile/change_password_page.dart
+++ lib/screens/profile/change_password_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 // Color Palettes
 Color primary = const Color(0xFF12215C);
 Color secondary = const Color(0xFF489DD6);
-Color tertiary = const Color(0xFFA7ADC3);
 Color white = const Color(0xFFFFFFFF);
 
 // Font Styles
@@ -14,6 +13,19 @@ TextStyle buttonFont1 = TextStyle(
   color: white,
 );
 
+TextStyle dialogTitleFont = TextStyle(
+  fontFamily: 'Poppins',
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+  color: primary,
+);
+
+TextStyle dialogContentFont = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 13,
+  color: primary,
+);
+
 ButtonStyle longButton1 = ElevatedButton.styleFrom(
   minimumSize: const Size(double.infinity, 50),
   backgroundColor: secondary,
@@ -22,17 +34,12 @@ ButtonStyle longButton1 = ElevatedButton.styleFrom(
   ),
 );
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: PasswordChangePage(),
-    );
-  }
-}
+ButtonStyle dialogButtonStyle = ElevatedButton.styleFrom(
+  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(8),
+  ),
+);
 
 class PasswordChangePage extends StatefulWidget {
   @override
@@ -44,27 +51,68 @@ class _PasswordChangePageState extends State<PasswordChangePage> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Simpan Perubahan', style: buttonFont1.copyWith(color: primary)),
-          content: Text(
-            'Apakah kamu yakin ingin menyimpan perubahan?',
-            style: buttonFont1.copyWith(fontSize: 13, color: primary), 
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Kembali', style: buttonFont1.copyWith(color: primary)),
+          child: Container(
+            width: 320, 
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Simpan Perubahan',
+                  textAlign: TextAlign.center,
+                  style: dialogTitleFont,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Apakah kamu yakin ingin menyimpan perubahan?',
+                  textAlign: TextAlign.center,
+                  style: dialogContentFont,
+                ),
+                const SizedBox(height: 50), 
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: primary,
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Kembali', style: buttonFont1),
+                      ),
+                    ),
+                    const SizedBox(width: 20), 
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: secondary,
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          _showSuccessDialog();
+                        },
+                        child: Text('Simpan', style: buttonFont1),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _showSuccessDialog();
-              },
-              child: Text('Simpan', style: buttonFont1.copyWith(color: secondary)),
-            ),
-          ],
+          ),
         );
       },
     );
@@ -75,13 +123,16 @@ class _PasswordChangePageState extends State<PasswordChangePage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Icon(Icons.check_circle, color: secondary, size: 60),
               const SizedBox(height: 10),
-              Text('Kata Sandi Berhasil diperbarui!', style: buttonFont1.copyWith(color: primary)),
+              Text('Kata Sandi Berhasil diperbarui!',
+                  style: buttonFont1.copyWith(color: primary)),
             ],
           ),
           actions: [
@@ -100,83 +151,91 @@ class _PasswordChangePageState extends State<PasswordChangePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: white,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        backgroundColor: primary,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Icon(Icons.arrow_back, color: white), // Arrow back icon
-            ),
-            const SizedBox(height: 10), // Space between icon and text
-            Text(
-              'Ubah Kata Sandi Baru',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: white,
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Stack(
+      backgroundColor: primary,
+      body: Column(
         children: [
-          // Latar belakang primary lebih panjang
           Container(
-            color: primary,
-            height: 80,
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 80),
-            decoration: BoxDecoration(
-              color: white,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).padding.top + 16,
+              left: 16,
+              right: 16,
+              bottom: 16,
             ),
-            padding: const EdgeInsets.all(20.0),
+            color: primary,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(Icons.lock, color: secondary, size: 100),
-                const SizedBox(height: 20),
-                Text(
-                  'Masukkan kata sandi baru',
-                  style: buttonFont1.copyWith(color: primary, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Kata sandi baru',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Konfirmasi kata sandi baru',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.arrow_back, color: white),
                   ),
                 ),
                 const SizedBox(height: 20),
-                ElevatedButton(
-                  style: longButton1,
-                  onPressed: _showConfirmationDialog,
-                  child: Text('Simpan Perubahan', style: buttonFont1),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Buat Kata Sandi Baru',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: white,
+                    ),
+                  ),
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(top: 50),
+              decoration: BoxDecoration(
+                color: white,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.lock, color: secondary, size: 100),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Masukkan kata sandi baru',
+                    style: buttonFont1.copyWith(
+                        color: primary, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 25),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Kata sandi baru',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Konfirmasi kata sandi baru',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    style: longButton1,
+                    onPressed: _showConfirmationDialog,
+                    child: Text('Simpan Perubahan', style: buttonFont1),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
