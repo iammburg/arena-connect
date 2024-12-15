5f86f0394f1a4eb9b7e443cc3c0f290854b3da48
MilaDewii <milanrhd13@gmail.com>
2024-12-15 16:14:17 +0700

    feat : update history

--- lib/screens/history/history_detail.dart
+++ lib/screens/history/history_detail.dart
@@ -1,6 +1,6 @@
-import 'package:arena_connect/config/theme.dart';
 import 'package:flutter/material.dart';
 
+// Color Palettes
 Color primary = const Color(0xFF12215C);
 Color tertiary = const Color(0xFFA7ADC3);
 Color white = const Color(0xFFFFFFFF);
@@ -19,34 +19,14 @@ class _HistoryDetailState extends State<HistoryDetail> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-          title: Text(
-            'Terima Kasih!',
-            style: superFont1,
-          ),
-          content: Container(
-            width: 30.0,
-            height: 40.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Terima kasih atas rating Anda!',
-                  style: superFont4,
-                ),
-              ],
-            ),
-          ),
+          title: const Text('Terima Kasih!'),
+          content: const Text('Terima kasih atas rating Anda!'),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text(
-                'Close',
-                style: superFont3,
-              ),
+              child: const Text('Close'),
             ),
           ],
         );
@@ -81,114 +61,120 @@ class _HistoryDetailState extends State<HistoryDetail> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'History Pertandingan',
-          style: superFont1.copyWith(color: white),
+          'History Pembayaran',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: white,
+          ),
         ),
         backgroundColor: primary,
         leading: IconButton(
-          icon: const Icon(
-            Icons.chevron_left,
-            color: Colors.white,
-            size: 30,
-          ),
+          icon: Icon(Icons.arrow_back_ios, color: white), // Ikon back
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.of(context).pop(); // Aksi kembali
           },
         ),
-        titleSpacing: 0.0,
+        leadingWidth: 20, // Atur lebar area leading agar lebih kecil
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(20.0),
+          preferredSize: const Size.fromHeight(6.0),
           child: Container(
             height: 4.0,
           ),
         ),
       ),
-      body: Column(
+            body: Column(
         children: [
           Container(
+            padding: const EdgeInsets.all(16.0),
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 35),
-                  Text('Tanding Berani?', style: superFont1),
-                  const SizedBox(height: 5),
-                  Text('Bulusan, Tembalang, Kota Semarang',
-                      style: regulerFont1),
+                  Text(
+                    'GOR LOMBA TRI JUANG',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Lapangan Lomba Tri Juang 2A',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: primary,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
                 ],
               ),
             ),
           ),
-          const SizedBox(height: 5),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(30.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
+                  const SizedBox(height: 20),
                   Card(
-                    elevation: 2,
+                    elevation: 5,
                     child: Padding(
-                      padding: const EdgeInsets.all(30.0),
+                      padding: const EdgeInsets.all(16.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Row(
                             children: [
-                              const Icon(
-                                Icons.sports_tennis,
-                                color: Color(0xFF12215C),
-                                size: 25,
-                              ),
-                              const SizedBox(width: 20),
+                              const Icon(Icons.sports_tennis),
+                              const SizedBox(width: 10),
                               Text(
-                                'Badminton - semua level',
-                                style: superFont4,
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 10),
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.calendar_today,
-                                color: Color(0xFF12215C),
-                                size: 24,
-                              ),
-                              const SizedBox(width: 20),
-                              Text(
-                                'Minggu, 22 September 2024\n10:00 - 12:00',
-                                style: superFont4,
-                              )
-                            ],
-                          ),
-                          const SizedBox(height: 10),
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.location_on,
-                                color: Color(0xFF12215C),
-                                size: 25,
-                              ),
-                              const SizedBox(width: 20),
-                              Text(
-                                'Lapangan Badminton Bulusan\nLapangan 2',
-                                style: superFont4,
+                                'Badminton',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ],
                           ),
                           const SizedBox(height: 10),
                           Row(
                             children: [
-                              const Icon(
-                                Icons.monetization_on,
-                                color: Color(0xFF12215C),
-                                size: 25,
-                              ),
-                              const SizedBox(width: 20),
+                              const Icon(Icons.calendar_today),
+                              const SizedBox(width: 10),
                               Text(
-                                'Biaya: 60.000',
-                                style: superFont4,
+                                'Jum, 5 Dec 2024\n09:00 - 10:00',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              const Icon(Icons.location_on),
+                              const SizedBox(width: 10),
+                              Text(
+                                'Jl. Tri Lomba Juang, Mugassari',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              const Icon(Icons.monetization_on),
+                              const SizedBox(width: 10),
+                              Text(
+                                'Biaya: 26.000',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
                               ),
                             ],
                           ),
@@ -196,10 +182,13 @@ class _HistoryDetailState extends State<HistoryDetail> {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 40),
+                  const SizedBox(height: 20),
                   Text(
                     'Bagaimana Pengalamanmu?',
-                    style: superFont2,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -215,6 +204,3 @@ class _HistoryDetailState extends State<HistoryDetail> {
     );
   }
 }
-//
-
-
